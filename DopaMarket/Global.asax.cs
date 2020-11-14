83bbad0e662ec5ed8349b8997846dce257013c5c
Sébastien Cadiot <cadiot@Immersion.local>
2020-11-14 10:33:03 +0100

    Ajout de quelques fonctionnalité

--- DopaMarket/Global.asax.cs
+++ DopaMarket/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DopaMarket.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
--- DopaMarket/Scripts/dopamarket-scripts.js
+++ DopaMarket/Scripts/dopamarket-scripts.js
@@ -23,13 +23,23 @@ jQuery(document).ready(function ($) {
             $(".topbar-cart-count-label").each(function (index) {
                 $(this).text(response.Items.length);
             })
+            var total = 0;
             for (var i = response.Items.length - 1; i >= 0; i--) {
                 var item = response.Items[i];
                 var newItem = cartDropdownItem.clone();
                 var link = newItem.find(".cart-dropdown-item-link");
+                var image = newItem.find(".cart-dropdown-item-image");
+                var info = newItem.find(".cart-dropdown-item-info");
                 link.text(item.Item.Name);
+                link.attr("href", "/Item/" + item.Item.LinkName);
+                image.attr("src", "/img/items/" + item.Item.LinkName + "_0.jpg");
+                info.text(item.Quantity + " x " + item.Item.CurrentPrice + " €");
                 newItem.prependTo(cartDropdownItemsContainer);
+                total += item.Quantity * item.Item.CurrentPrice;
             }
+            $(".cart-dropdown-total").each(function (index) {
+                $(this).text((Math.round(total * 100) / 100)+ " €");
+            })
         });
     }
 
